04c68d92aba1617e3fc197d1f63d05040cce677d
NMWXask <k.marcus@mail.ru>
2025-06-10 22:16:18 +0400

    lesson1_task3.kt variables

---
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -0,0 +1,13 @@
+package ru.vtb.msa.lesson_1
+
+fun main() {
+    val year = 1961;
+    var hour = 0.9;
+    var minute = 0.7;
+    println(year)
+    println(hour)
+    println(minute)
+    hour = 10.0;
+    minute = 55.0;
+    println("Время посадки $hour : $minute")
+}
