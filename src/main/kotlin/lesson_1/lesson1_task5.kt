3292823d2bf08778f6076ea3220952dc7c124a88
NMWXask <k.marcus@mail.ru>
2025-06-10 22:30:53 +0400

    lesson1_task5.kt variables

---
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -0,0 +1,13 @@
+package ru.vtb.msa.lesson_1
+
+fun main() {
+    val totalSeconds = 6480
+
+    val hours = totalSeconds / 3600
+    val minutes = (totalSeconds % 3600) / 60
+    val seconds = totalSeconds % 60
+
+    val formattedTime = String.format("%02d:%02d:%02d", hours, minutes, seconds)
+
+    println("Время проведенное в космосе : $formattedTime")
+}
